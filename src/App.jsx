e965bc4f56e42391792b125971c84b3b83f27fa3
iceddrop <93996532+iceddrop@users.noreply.github.com>
2022-10-14 12:51:01 +0100

    made navbar responsive on mobile and desktop view

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import Navbar from './components/navbar'
-
+import Home from './components/Home'
 import './App.css'
 
 function App() {
   
-
   return (
-          <>
+          <div className='overall'>
              <Navbar/>
-          </>
+             <Home/>
+          </div>
   )
 }
 
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import logo from '../assets/logo.svg'
 import menu from '../assets/menu.svg'
-import home from '../assets/home.svg'
-import collections from '../assets/playlist.svg'
-import radio from '../assets/radio.svg'
+import search from '../assets/search.svg'
 import video from '../assets/videos.svg'
 import profile from '../assets/profile.svg'
 import logout from '../assets/Logout.svg'
+import { FaHome } from 'react-icons/fa'
+import { BsFillCollectionFill } from "react-icons/bs"
+import { IoMdRadio } from "react-icons/io";
+
 export default function Navbar(){
       const [clicked, setClicked] = React.useState(false)
 
@@ -14,25 +16,26 @@ export default function Navbar(){
       function clickAct(){
         setClicked(val => !val)
       }
+	  
 	return(
-		<nav className='nav'>
+		<nav className='nav p-4 fixed-top'>
 		   <div className='flex'>
-                <img src={menu} onClick={clickAct} className=''/>
-				<img src={logo} className='app-logo' />
-				<form>
-                   <input placeholder='search artists' className='search'/>
-				</form>			
-		  </div>
-		  <div className={clicked ? 'block' : 'hidden'}>
-			<div className='flex flex-row items-center'><img src={home} className=''/><h6>Home</h6></div>
-			<div className='flex flex-row items-center'><img src={collections} className=''/><h6>My collections</h6></div>
-			<div className='flex flex-row items-center'><img src={radio} className=''/><h6>Radio</h6></div>
-			<div className='flex flex-row items-center'><img src={video} className=''/><h6>Music video</h6></div>
-			<div>
-            <div className='flex flex-row items-center'><img src={profile} className=''/><h6>Profile</h6></div>
-            <div className='flex flex-row items-center'><img src={logout} className=''/><h6>Log out</h6></div>
+                <img src={menu} onClick={clickAct} className='hamburger md:hidden'/>
+				<img src={logo} className='app-logo pl-4' />
+				<form className='search-input flex justify-between md:flex-row-reverse md:items-center'>
+                   <input placeholder='search artists'  className='search ml-6'/>
+				   <img src={search} className='search-icon md:ml-12 lg:ml-14 2xl:ml-18' />
+				</form>	
 			</div>
-		  </div>		
+				<div className={clicked ? 'block nav-dropdown' : 'hidden'}>
+					<div className='flex flex-row items-center pt-4 pb-3 px-2 nav-item'><FaHome className='bar-icon'/><h6 className='pl-4'>Home</h6></div>
+					<div className='flex flex-row items-center px-2 py-3 nav-item'><BsFillCollectionFill className='bar-icon'/><h6 className='pl-4'>My collections</h6></div>
+					<div className='flex flex-row items-center px-2 py-3 nav-item'><IoMdRadio className='bar-icon'/><h6 className='pl-4'>Radio</h6></div>
+					<div className='flex flex-row items-center px-2 py-3 nav-item'><img src={video} className='bar-icon'/><h6 className='pl-4'>Music video</h6></div>
+					<div className='flex flex-row items-center px-2 py-3 nav-item'><img src={profile} className='bar-icon'/><h6 className='pl-4'>Profile</h6></div>
+					<div className='flex flex-row items-center px-2 py-3 nav-item'><img src={logout} className='bar-icon'/><h6 className='pl-4'>Log out</h6></div>
+				</div>				
 		</nav>
+		
 	)
 }
