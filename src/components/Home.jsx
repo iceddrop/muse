fb607e49741f5cd6e352bacf6f3cca40f0057309
iceddrop <93996532+iceddrop@users.noreply.github.com>
2022-10-20 22:39:05 +0100

    made hero section responsive across most devices

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,14 +15,14 @@ export default function Home(){
         <section className=''>
            <div class="sidebar  flex-col items-center hidden md:flex">
                   <div className='icon-div-1 flex flex-col items-center py-4 rounded-lg'>
-                      <FaHome className='bar-icon mb-4'/>
-                      <BsFillCollectionFill className='bar-icon mb-4'/>
-                      <IoMdRadio className='bar-icon mb-4'/>
-                      <img src={video} className='bar-icon' alt='video-icon'/>
+                      <FaHome className='bar-icon mb-4 cursor-pointer'/>
+                      <BsFillCollectionFill className='bar-icon mb-4 cursor-pointer'/>
+                      <IoMdRadio className='bar-icon mb-4 cursor-pointer'/>
+                      <img src={video} className='bar-icon cursor-pointer' alt='video-icon'/>
                 </div>
                 <div className='icon-div-2 flex flex-col items-center mt-4 py-4'>
-                      <img src={profile} className='bar-icon mb-3' alt='profile-icon'/>
-                      <img src={logout} className='bar-icon ' alt='logout-icon'/>
+                      <img src={profile} className='bar-icon mb-3 cursor-pointer' alt='profile-icon'/>
+                      <img src={logout} className='bar-icon cursor-pointer' alt='logout-icon'/>
                 </div>
             </div>
             <div className='hero-div lg:flex'>
@@ -30,11 +30,11 @@ export default function Home(){
                   <img src={smallHeroImg} className='md:hidden hero-img mb-4'/>
                      <img src={heroImg} className='hero-img hidden md:flex '/>
                         <div className='hero-section-text text-white flex flex-col justify-between pb-8 items-start'>
-                           <h3 className='mt-2'>Currated playlist</h3> 
+                           <h3 className='mt-2 hero-section-title'>Currated playlist</h3> 
                            <div className='mini-hero-div flex flex-col justify-between'>
                                <div className=''>
-                                   <h1 className='mb-4'>R & B Hits</h1>
-                                   <p className='hero-paragraph'>All mine, Lie again, Petty call me everyday, Out of time, No love, Bad habit and so much more.</p>
+                                   <h1 className='mb-3 hero-section-title-2'>R & B Hits</h1>
+                                   <p className='mb-4 hero-paragraph'>All mine, Lie again, Petty call me everyday, Out of time, No love, Bad habit and so much more.</p>
                                 </div> 
                                 <div className='flex'>
                                    <img src={musicdisk} className='music-disk' alt='music-disk'/>
@@ -44,42 +44,48 @@ export default function Home(){
                            </div>   
                      </div>
                   </div>
-                  <div className='text-white'>
-                     <h1 className='pb-3'>Top Charts</h1>
+                  <div className='text-white lg:ml-4'>
+                     <h1 className='pb-3 top-chart lg:ml-4'>Top Charts</h1>
                      <div className='carousel '> 
                      <div className='inner-carousel lg:flex lg:flex-col'>
-                        <div className='music-chart-card flex justify-between p-4 lg:ml-4'>
+                        <div className='music-chart-card flex justify-between p-3 lg:ml-4'>
                               <div className='lg:flex'>
                                  <img src={rectangle} className='rectangle' alt='music-artwork'/>
-                                 <div className='mt-3 music-info'>
+                                 <div className='music-info lg:ml-4'>
                                     <h3 className='chart-card-header'>Golden age of 90's</h3>
                                     <h6 className='chart-card-artist'>Nivarna</h6>
-                                    <p className='mt-3 chart-card-playtime'>2:10:45</p>
+                                    <p className=' chart-card-playtime'>2:10:45</p>
                                  </div>  
                               </div>
-                              <img src={heart2} className='heart2  p-1' alt='heart-icon'/>  
+                              <div className='heart-icon-div'>
+                                   <img src={heart2} className='heart2  p-1' alt='heart-icon'/> 
+                              </div>   
                         </div>
-                        <div className='music-chart-card flex justify-between p-4 ml-4'>
+                        <div className='music-chart-card flex justify-between p-3 ml-4'>
                               <div className='lg:flex'>
                                  <img src={rectangle} className='rectangle' alt='music-artwork'/>
-                                 <div className='mt-3 music-info'>
+                                 <div className='music-info lg:ml-4'>
                                     <h3 className='chart-card-header'>Golden age of 90's</h3>
                                     <h6 className='chart-card-artist'>Nivarna</h6>
-                                    <p className='mt-3 chart-card-playtime'>2:10:45</p>
+                                    <p className='chart-card-playtime'>2:10:45</p>
                                  </div>  
                               </div>
-                              <img src={heart2} className='heart2  p-1' alt='heart-icon'/>  
+                              <div className='heart-icon-div'>
+                                   <img src={heart2} className='heart2  p-1' alt='heart-icon'/> 
+                              </div>  
                         </div>
-                        <div className='music-chart-card flex justify-between p-4 ml-4'>
+                        <div className='music-chart-card flex justify-between p-3 ml-4'>
                               <div className='lg:flex'>
                                  <img src={rectangle} className='rectangle' alt='music-artwork'/>
-                                 <div className='mt-3 music-info'>
+                                 <div className='music-info lg:ml-4'>
                                     <h3 className='chart-card-header'>Golden age of 90's</h3>
                                     <h6 className='chart-card-artist'>Nivarna</h6>
-                                    <p className='mt-3 chart-card-playtime'>2:10:45</p>
+                                    <p className='chart-card-playtime'>2:10:45</p>
                                  </div>  
                               </div>
-                              <img src={heart2} className='heart2  p-1' alt='heart-icon'/>  
+                              <div className='heart-icon-div'>
+                                   <img src={heart2} className='heart2  p-1' alt='heart-icon'/> 
+                              </div> 
                         </div>
                      </div>
                      </div>
