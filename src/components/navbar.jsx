65844f8ac419d36f493724b7ffbafdc55fb229b6
iceddrop <93996532+iceddrop@users.noreply.github.com>
2022-10-24 22:32:59 +0100

    added artist search result dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,12 +10,37 @@ import { BsFillCollectionFill } from "react-icons/bs"
 import { IoMdRadio } from "react-icons/io";
 
 export default function Navbar(){
+	 const [artisteData, setArtisteData] = React.useState([])
+
       const [clicked, setClicked] = React.useState(false)
+
+	  const [searchInput, setSearchInput] =  React.useState('')
 
 {/*flip clicked state*/}
       function clickAct(){
         setClicked(val => !val)
       }
+
+	 function searchArtistes(){
+		const options = {
+			method: 'GET',
+			headers: {
+				'X-RapidAPI-Key': '036795ec2amsh8c2b98ef8a502acp146724jsn6f3538b26522',
+				'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+			}
+		};
+
+		fetch(`https://spotify23.p.rapidapi.com/search/?q=${searchInput}&type=multi&offset=0&limit=10&numberOfTopResults=5`, options)
+		.then(response => response.json())
+		.then(response => setArtisteData(response.artists.items.map(artist=>artist.data.profile.name)))
+		.catch(err => console.error(err));
+	 }
+
+	function handleChange(event){
+		setSearchInput(event.target.value)
+	}
+console.log(searchInput)
+console.log(artisteData)
 	  
 	return(
 		<nav className='nav p-4 fixed-top'>
@@ -23,9 +48,16 @@ export default function Navbar(){
                 <img src={menu} onClick={clickAct} className='hamburger md:hidden'/>
 				<img src={logo} className='app-logo pl-4' />
 				<form className='search-input flex justify-between md:flex-row-reverse md:items-center'>
-                   <input placeholder='search artists'  className='search ml-6 '/>
+					<div className='flex flex-col'>
+							<input placeholder='search artists'  onChange={()=>{handleChange(event);searchArtistes()}} className='search ml-6 '/>
+									<ul className='flex flex-col text-white'>
+										{artisteData.map(name =>(
+                                               <li>{name}</li>
+										))}
+									</ul>										
+					</div>
 				   <img src={search} className='search-icon md:ml-12 lg:ml-14 2xl:ml-18' />
-				</form>	
+				</form>
 			</div>
 				<div className={clicked ? 'block nav-dropdown' : 'hidden'}>
 					<div className='flex flex-row items-center pt-4 pb-3 px-2 nav-item'><FaHome className='bar-icon'/><h6 className='pl-4'>Home</h6></div>
