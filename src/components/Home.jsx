11d21432a42ffa29a39c37b1d2d9352b6f2d78b4
iceddrop <93996532+iceddrop@users.noreply.github.com>
2022-11-05 22:02:56 +0100

    finished chart overview ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,112 +1,180 @@
-import video from '../assets/videos.svg'
-import profile from '../assets/profile.svg'
-import logout from '../assets/Logout.svg'
-import { FaHome } from 'react-icons/fa'
-import { BsFillCollectionFill } from "react-icons/bs"
-import { IoMdRadio } from "react-icons/io"
-import heart from '../assets/Heart.svg'
-import musicdisk from '../assets/musiccards.svg'
-import heart2 from '../assets/Heart2.svg'
-import rectangle from '../assets/Rectangle.svg'
-import heroImg from '../assets/HeroSection.svg'
-import smallHeroImg from '../assets/homesecmobile.svg'
-import React from 'react'
+import video from "../assets/videos.svg";
+import profile from "../assets/profile.svg";
+import logout from "../assets/Logout.svg";
+import { FaHome } from "react-icons/fa";
+import { BsFillCollectionFill } from "react-icons/bs";
+import { IoMdRadio } from "react-icons/io";
+import heart from "../assets/Heart.svg";
+import musicdisk from "../assets/musiccards.svg";
+import heart2 from "../assets/Heart2.svg";
+import rectangle from "../assets/Rectangle.svg";
+import heroImg from "../assets/HeroSection.svg";
+import smallHeroImg from "../assets/homesecmobile.svg";
+import React from "react";
 import { useContext } from "react";
 import { SearchContext } from "../contexts/SearchContext";
-import ChartOverview from './chartOverview'
+import ChartOverview from "./chartOverview";
 
+export default function Home() {
+  const { chartDisplayed, setChartDisplayed } = useContext(SearchContext);
 
-
-export default function Home(){
-   const {chartDisplayed, setChartDisplayed} = useContext(SearchContext)
-
-
-    return(
-        <section>
-           <div className="sidebar  flex-col items-center hidden md:flex">
-                  <div className='icon-div-1 flex flex-col items-center py-4 rounded-3xl'>
-                      <FaHome className='bar-icon mb-4 cursor-pointer' onClick={() => {setChartDisplayed(false)}}/>
-                      <BsFillCollectionFill className='bar-icon mb-4 cursor-pointer'/>
-                      <IoMdRadio className='bar-icon mb-4 cursor-pointer'/>
-                      <img src={video} className='bar-icon cursor-pointer' alt='video-icon'/>
+  return (
+    <section>
+      <div className="sidebar  flex-col items-center hidden md:flex">
+        <div className="icon-div-1 flex flex-col items-center py-4 rounded-3xl">
+          <FaHome
+            className="bar-icon mb-4 cursor-pointer"
+            onClick={() => {
+              setChartDisplayed(false);
+            }}
+          />
+          <BsFillCollectionFill className="bar-icon mb-4 cursor-pointer" />
+          <IoMdRadio className="bar-icon mb-4 cursor-pointer" />
+          <img
+            src={video}
+            className="bar-icon cursor-pointer"
+            alt="video-icon"
+          />
+        </div>
+        <div className="icon-div-2 flex flex-col items-center mt-4 py-4 rounded-3xl">
+          <img
+            src={profile}
+            className="bar-icon mb-3 cursor-pointer"
+            alt="profile-icon"
+          />
+          <img
+            src={logout}
+            className="bar-icon cursor-pointer"
+            alt="logout-icon"
+          />
+        </div>
+      </div>
+      {chartDisplayed ? (
+        <ChartOverview />
+      ) : (
+        <div className="hero-div lg:flex">
+          <div className="hero-accomodate">
+            <img src={smallHeroImg} className="md:hidden hero-img mb-4" />
+            <img src={heroImg} className="hero-img hidden md:flex " />
+            <div className="hero-section-text text-white flex flex-col justify-between pb-8 items-start">
+              <h3 className="mt-2 hero-section-title">Currated playlist</h3>
+              <div className="mini-hero-div flex flex-col justify-between">
+                <div className="">
+                  <h1 className="mb-3 hero-section-title-2">R & B Hits</h1>
+                  <p className="mb-4 hero-paragraph">
+                    All mine, Lie again, Petty call me everyday, Out of time, No
+                    love, Bad habit and so much more.
+                  </p>
                 </div>
-                <div className='icon-div-2 flex flex-col items-center mt-4 py-4 rounded-3xl'>
-                      <img src={profile} className='bar-icon mb-3 cursor-pointer' alt='profile-icon'/>
-                      <img src={logout} className='bar-icon cursor-pointer' alt='logout-icon'/>
+                <div className="flex">
+                  <img
+                    src={musicdisk}
+                    className="music-disk"
+                    alt="music-disk"
+                  />
+                  <img
+                    src={heart}
+                    className="heart-icon ml-2"
+                    alt="heart-icon"
+                  />
+                  <p className="ml-2 mt-1">33k likes</p>
                 </div>
+              </div>
             </div>
-            {
-               chartDisplayed ?  
-               <ChartOverview />
-               :
-               <div className='hero-div lg:flex'>
-               <div className='hero-accomodate'>
-               <img src={smallHeroImg} className='md:hidden hero-img mb-4'/>
-                  <img src={heroImg} className='hero-img hidden md:flex '/>
-                     <div className='hero-section-text text-white flex flex-col justify-between pb-8 items-start'>
-                        <h3 className='mt-2 hero-section-title'>Currated playlist</h3> 
-                        <div className='mini-hero-div flex flex-col justify-between'>
-                            <div className=''>
-                                <h1 className='mb-3 hero-section-title-2'>R & B Hits</h1>
-                                <p className='mb-4 hero-paragraph'>All mine, Lie again, Petty call me everyday, Out of time, No love, Bad habit and so much more.</p>
-                             </div> 
-                             <div className='flex'>
-                                <img src={musicdisk} className='music-disk' alt='music-disk'/>
-                                <img src={heart} className='heart-icon ml-2' alt='heart-icon'/>
-                                <p className='ml-2 mt-1'>33k likes</p>
-                            </div> 
-                        </div>   
+          </div>
+          <div className="text-white lg:ml-4">
+            <h1 className="pb-3 top-chart lg:ml-4">Top Charts</h1>
+            <div className="carousel ">
+              <div className="inner-carousel lg:flex lg:flex-col">
+                <div className="music-chart-card flex justify-between p-3 lg:ml-4 cursor-pointer">
+                  <div className="lg:flex">
+                    <img
+                      src={rectangle}
+                      className="rectangle"
+                      alt="music-artwork"
+                    />
+                    <div className="music-info lg:ml-4">
+                      <h3
+                        className="chart-card-header"
+                        onClick={() => {
+                          setChartDisplayed(true);
+                        }}
+                      >
+                        Golden age of 90's
+                      </h3>
+                      <h6 className="chart-card-artist">Nivarna</h6>
+                      <p className=" chart-card-playtime">2:10:45</p>
+                    </div>
                   </div>
-               </div>
-               <div className='text-white lg:ml-4'>
-                  <h1 className='pb-3 top-chart lg:ml-4'>Top Charts</h1>
-                  <div className='carousel '> 
-                  <div className='inner-carousel lg:flex lg:flex-col'>
-                     <div className='music-chart-card flex justify-between p-3 lg:ml-4 cursor-pointer'>
-                           <div className='lg:flex'>
-                              <img src={rectangle} className='rectangle' alt='music-artwork'/>
-                              <div className='music-info lg:ml-4'>
-                                 <h3 className='chart-card-header' onClick={() =>{setChartDisplayed(true)}}>Golden age of 90's</h3>
-                                 <h6 className='chart-card-artist'>Nivarna</h6>
-                                 <p className=' chart-card-playtime'>2:10:45</p>
-                              </div>  
-                           </div>
-                           <div className='heart-icon-div'>
-                                <img src={heart2} className='heart2  p-1' alt='heart-icon'/> 
-                           </div>   
-                     </div>
-                     <div className='music-chart-card flex justify-between p-3 ml-4 cursor-pointer'>
-                           <div className='lg:flex'>
-                              <img src={rectangle} className='rectangle' alt='music-artwork'/>
-                              <div className='music-info lg:ml-4'>
-                                 <h3 className='chart-card-header' onClick={() =>{setChartDisplayed(true)}}>Golden age of 90's</h3>
-                                 <h6 className='chart-card-artist'>Nivarna</h6>
-                                 <p className='chart-card-playtime'>2:10:45</p>
-                              </div>  
-                           </div>
-                           <div className='heart-icon-div'>
-                                <img src={heart2} className='heart2  p-1' alt='heart-icon'/> 
-                           </div>  
-                     </div>
-                     <div className='music-chart-card flex justify-between p-3 ml-4 cursor-pointer'>
-                           <div className='lg:flex'>
-                              <img src={rectangle} className='rectangle' alt='music-artwork'/>
-                              <div className='music-info lg:ml-4'>
-                                 <h3 className='chart-card-header' onClick={() =>{setChartDisplayed(true)}}>Golden age of 90's</h3>
-                                 <h6 className='chart-card-artist'>Nivarna</h6>
-                                 <p className='chart-card-playtime'>2:10:45</p>
-                              </div>  
-                           </div>
-                           <div className='heart-icon-div'>
-                                <img src={heart2} className='heart2  p-1' alt='heart-icon'/> 
-                           </div> 
-                     </div>
+                  <div className="heart-icon-div">
+                    <img
+                      src={heart2}
+                      className="heart2  p-1"
+                      alt="heart-icon"
+                    />
                   </div>
+                </div>
+                <div className="music-chart-card flex justify-between p-3 ml-4 cursor-pointer">
+                  <div className="lg:flex">
+                    <img
+                      src={rectangle}
+                      className="rectangle"
+                      alt="music-artwork"
+                    />
+                    <div className="music-info lg:ml-4">
+                      <h3
+                        className="chart-card-header"
+                        onClick={() => {
+                          setChartDisplayed(true);
+                        }}
+                      >
+                        Golden age of 90's
+                      </h3>
+                      <h6 className="chart-card-artist">Nivarna</h6>
+                      <p className="chart-card-playtime">2:10:45</p>
+                    </div>
                   </div>
-               </div>      
-         </div>
-            }
- 
-        </section> 
-    )
+                  <div className="heart-icon-div">
+                    <img
+                      src={heart2}
+                      className="heart2  p-1"
+                      alt="heart-icon"
+                    />
+                  </div>
+                </div>
+                <div className="music-chart-card flex justify-between p-3 ml-4 cursor-pointer">
+                  <div className="lg:flex">
+                    <img
+                      src={rectangle}
+                      className="rectangle"
+                      alt="music-artwork"
+                    />
+                    <div className="music-info lg:ml-4">
+                      <h3
+                        className="chart-card-header"
+                        onClick={() => {
+                          setChartDisplayed(true);
+                        }}
+                      >
+                        Golden age of 90's
+                      </h3>
+                      <h6 className="chart-card-artist">Nivarna</h6>
+                      <p className="chart-card-playtime">2:10:45</p>
+                    </div>
+                  </div>
+                  <div className="heart-icon-div">
+                    <img
+                      src={heart2}
+                      className="heart2  p-1"
+                      alt="heart-icon"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </section>
+  );
 }
--- src/components/chartOverview.jsx
+++ src/components/chartOverview.jsx
@@ -1,26 +1,26 @@
 import LeadImage from '../assets/Lead-image.svg'
 import MusicSquare from '../assets/music-square-add.svg'
 import redHeart from '../assets/red-heart.svg'
-import playTwo from '../assets/Play-2.svg'
+import playTwo from '../assets/chartPlay.svg'
 import whiteHeart from '../assets/white-heart.svg'
 import coverart from '../assets/coverart.svg'
 import more from '../assets/more-vertical.svg'
 export default function showOverview(){
     return(
-       <section className="chart-category text-white pr-6">
+       <section className="chart-category text-white pr-8">
         <div className=' md:flex'>
             <div className='md:flex'>
                 <img src={LeadImage} className='chart-img rounded-3xl' alt='chart-image'/>
-                <div className='pr-4 mt-4 md:pt-14 md:ml-6 xl:pt-20 lg:pr-10'>
+                <div className=' mt-4 md:pt-14 md:ml-6 xl:pt-20 lg:pr-10'>
                     <h2 className='chart-header'>Tommorrow's tunes</h2>
                     <p className='chart-paragraph mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellat sapiente sequi dolore commodi.</p>
                     <div className='flex mt-1 chart-duration'>
                         <p>64 songs -</p>
                         <p> 16 hrs+</p>
                     </div>
-                    <div className='flex mt-4 '>
+                    <div className='flex mt-4'>
                         <div className='flex chart-icon-div cursor-pointer'>
-                            <img src={playTwo} className='' alt='play-icon'/>
+                            <img src={redHeart} className='red-heart' alt='heart-icon'/>
                             <p className='ml-2'>Play all</p>
                         </div>
                         <div className='flex chart-icon-div ml-3 cursor-pointer'>
@@ -28,7 +28,7 @@ export default function showOverview(){
                             <p className='ml-2'>Add to collection</p>
                         </div>
                         <div className='flex chart-icon-div ml-3 '>
-                            <img src={redHeart} className='' alt='heart-icon'/>
+                            <img src={playTwo} className='' alt='play-icon'/>
                             <p className='ml-2 md:hidden'>Like</p>
                         </div>
                     </div>
@@ -39,13 +39,13 @@ export default function showOverview(){
             <div className='chart-songs-div flex justify-between items-center px-2'>
                 <div className='flex'>
                     <img src={coverart} className='chart-coverart' alt='song-coverart'/>
-                    <img src={whiteHeart} className='hidden md:block white-heart ml-3 mt-2' alt='white-heart'/>
+                    <img src={whiteHeart} className='hidden md:block white-heart ml-4 mt-2' alt='white-heart'/>
                 </div>
-                <div className='md:flex'>
+                <div className='md:flex justify-between md:w-80 md:itrems-center'>
                     <h3 className='chart-song-title'>Song title</h3>
                     <h5 className='chart-artsite'>Artist</h5>
                 </div>
-                <div className='md:flex md:flex-row-reverse'>
+                <div className='flex flex-col  md:flex md:flex-row-reverse md:w-60 md:justify-between md:pr-4'>
                    <img src={more} className='more-icon' alt='more-icon'/>
                     <p className='song-duration'>4:17</p>
                 </div>
